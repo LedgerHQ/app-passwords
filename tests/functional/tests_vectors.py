7887683423046a3a22d5eb348e0599ec70cd1956
Lucas PASCAL <lucas.pascal@ledger.com>
2023-04-06 09:15:04 +0200

    [clean] Review: Workflows

--- tests/functional/tests_vectors.py
+++ tests/functional/tests_vectors.py
@@ -30,14 +30,18 @@ tests_vectors = {
     ],
 
     "test_load_metadatas_with_too_much_data": [
-        b"\x00" * 10000,
-        bytes.fromhex("02000761060007616c6c6168") + b"\x00" * 4096,
+        # 1-element array to avoid huge test names filled with the data.
+        # Instead, it is filled with the data index
+        [b"\x00" * 10000],
+        [bytes.fromhex("02000761060007616c6c6168") + b"\x00" * 4096],
     ],
 
     "test_load_metadatas_with_name_too_long": [
-        bytes.fromhex(
-            "02000761" "15 00 07 616c6c6168616c6c6168616c6c6168616c6c7078"),
-        bytes.fromhex("02000761" "15 00 07 616c6c6168616c6c6168616c6c6168616c6c7078") +
-        b"\x00" * (4096 - 27),
+        # 1-element array to avoid huge test names filled with the data.
+        # Instead, it is filled with the data index
+        [bytes.fromhex(
+            "02000761" "15 00 07 616c6c6168616c6c6168616c6c6168616c6c7078")],
+        [bytes.fromhex("02000761" "15 00 07 616c6c6168616c6c6168616c6c6168616c6c7078") +
+         b"\x00" * (4096 - 27)],
     ],
 }
