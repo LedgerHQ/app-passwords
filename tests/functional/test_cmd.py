dd40e15610a7726e920ce93713558de2dd734d55
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2026-06-05 17:20:35 +0200

    Bump version to 1.3.2

--- tests/functional/test_cmd.py
+++ tests/functional/test_cmd.py
@@ -2,7 +2,7 @@ from passwordsManager_cmd import PasswordsManagerCommand
 
 
 def test_app_info(cmd: PasswordsManagerCommand):
-    assert cmd.get_app_info() == ("Passwords", "1.3.1")
+    assert cmd.get_app_info() == ("Passwords", "1.3.2")
 
 
 def test_app_config(cmd: PasswordsManagerCommand):
